12db4ac520057d9f255f84560ecdcc51a6ec5fd2
alex <acontell@gmail.com>
2021-08-26 07:01:39 -0400

    backend application up and running

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,31 +4,32 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/) && req.get?
-      items = Item.all
-      return [200, { 'Content-Type' => 'application/json' }, [ items.to_json ]]
+    if req.path.match(/rolls/) && req.get?
+      rolls = Roll.all
+      return [200, { 'Content-Type' => 'application/json' }, [ rolls.to_json ]]
 
-    elsif req.path.match(/items/) && req.post?
+    elsif req.path.match(/rolls/) && req.post?
       data = JSON.parse req.body.read
-      item = Item.create(data)
-      return [200, { 'Content-Type' => 'application/json' }, [ item.to_json ]]
-
-
-    elsif req.path.match(/users/) && req.get?
-      users = User.all
-      return [200, { 'Content-Type' => 'application/json' }, [ users.to_json ]]
-
-
-    elsif req.path.match(/categories/) && req.get?
-      categories = Category.all
-      return [200, { 'Content-Type' => 'application/json' }, [ categories.to_json ]]
-
-
+      roll = Roll.create(data)
+      return [200, { 'Content-Type' => 'application/json' }, [ roll.to_json ]]
+      
     elsif req.delete?
-      id = req.path_info.split('/items/').last
-      item = Item.find(id)
-      item.delete
-      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Item deleted"}.to_json ]]
+      id = req.path_info.split('/rolls/').last
+      roll = Roll.find(id)
+      roll.delete
+      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Roll deleted"}.to_json ]]
+
+    elsif req.path.match(/brands/) && req.get?
+      brands = Brand.all
+      return [200, { 'Content-Type' => 'application/json' }, [ brands.to_json ]]
+
+
+    elsif req.path.match(/formats/) && req.get?
+      formats = Format.all
+      return [200, { 'Content-Type' => 'application/json' }, [ formats.to_json ]]
+
+
+
 
     else
       resp.write "Path Not Found"
