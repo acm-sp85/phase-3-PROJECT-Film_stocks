33f778c7e005f920f6e5e45fe96aff436e83d4f0
acm-sp85 <acontell@gmail.com>
2021-09-05 16:46:11 -0400

    Update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,19 +5,22 @@ import "../App.css";
 class Home extends React.Component {
   render() {
     return (
-      <Container className="container-fluid" className="home-card">
-        <Row>
-          <Col></Col>
-          <Col>
-            <Button variant="btn btn-outline-dark" href="rolls">
-              EXPLORE THE DATABASE
-            </Button>{" "}
-            <Button variant="btn btn-outline-dark" href="rolls/new">
-              ADD NEW FILMSTOCK
-            </Button>{" "}
-          </Col>
-          <Col></Col>
-        </Row>
+      <Container>
+        <h1 className="centered">The Film Photography Database</h1>
+        <Container className="container-fluid" className="home-card">
+          <Row>
+            <Col></Col>
+            <Col>
+              <Button variant="btn btn-outline-dark" href="rolls">
+                EXPLORE THE DATABASE
+              </Button>{" "}
+              <Button variant="btn btn-outline-dark" href="rolls/new">
+                ADD NEW FILMSTOCK
+              </Button>{" "}
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
       </Container>
     );
   }
